20de34990205c0499c28a9d6d70bf470a8aeeb87
Jacek Wójciak <fr.banka@gmail.com>
2023-01-23 17:20:58 +0100

    fetch countries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix, { Notify } from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(countrySearch, DEBOUNCE_DELAY));
+
+function countrySearch() {
+  const countryName = input.value.trim();
+
+  fetchCountries(countryName)
+    .then(showCountryInfo)
+    .catch(() => {
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
